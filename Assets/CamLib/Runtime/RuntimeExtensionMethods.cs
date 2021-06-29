afdf6432e49a189afdbd187946f4d2c7cb77afd4
Cammin <cameo221@gmail.com>
2021-06-29 00:12:43 -0700

    added extension methods

---
+++ Assets/CamLib/Runtime/RuntimeExtensionMethods.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace CamLib
+{
+    public static class RuntimeExtensionMethods
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return enumerable == null || !enumerable.Any();
+        }
+        
+        public static GameObject CreateChildGameObject(this GameObject parent, string name = "New GameObject")
+        {
+            GameObject child = new GameObject(name);
+            child.transform.SetParent(parent.transform);
+            child.transform.localPosition = Vector3.zero;
+            return child;
+        }
+    }
+}
