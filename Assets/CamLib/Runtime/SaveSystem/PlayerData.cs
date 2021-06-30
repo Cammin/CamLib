9c5e082ae47e2200618bae7247259d386f31f243
Cammin <55564581+Cammin@users.noreply.github.com>
2021-06-29 18:55:22 -0700

    altered protection of save data path

--- Assets/CamLib/Runtime/SaveSystem/PlayerData.cs
+++ Assets/CamLib/Runtime/SaveSystem/PlayerData.cs
@@ -5,7 +5,7 @@ namespace CamLib
     public static class PlayerData
     {
         private const string PLAYER_DATA_FILE_NAME = "playerdata.json";
-        public static string DataPath => $"{Application.persistentDataPath}/{PLAYER_DATA_FILE_NAME}";
+        internal static string DataPath => $"{Application.persistentDataPath}/{PLAYER_DATA_FILE_NAME}";
 
         public static T Load<T>() => PlayerDataLoad.LoadData<T>();
         public static void Save<T>(T data) => PlayerDataSave.SaveData(data);
