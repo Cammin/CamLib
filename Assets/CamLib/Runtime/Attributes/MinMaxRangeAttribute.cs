ac299678b877247589e5274547b3251cfbf4edfb
Cammin <cameo221@gmail.com>
2021-06-30 20:48:55 -0700

    added min max range attribute

---
+++ Assets/CamLib/Runtime/Attributes/MinMaxRangeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace CamLib
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class MinMaxRangeAttribute : PropertyAttribute
+    {
+        private const float DEFAULT_SNAP_THRESHOLD = 0.1f;
+        public MinMaxRangeAttribute(float min = 0, float max = 1, float snapThreshold = DEFAULT_SNAP_THRESHOLD)
+        {
+            Min = min;
+            Max = max;
+            SnapThreshold = snapThreshold;
+        }
+        public float Min { get; private set; }
+        public float Max { get; private set; }
+        public float SnapThreshold { get; private set; }
+    }
+}
