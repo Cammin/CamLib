96e300dc8360a5bb92360f5baec923cf54495031
Cammin <cameo221@gmail.com>
2021-07-07 00:38:05 -0700

    added debug log util

---
+++ Assets/CamLib/Runtime/Util/DebugLogUtil.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace CamLib
+{
+    // Use LOG_RELEASE_DISABLE in player settings to make this work
+    public static class DebugLogUtil
+    {
+#if LOG_RELEASE_DISABLE
+        [RuntimeInitializeOnLoadMethod]
+        private static void AllowDebugging()
+        {
+            Debug.unityLogger.logEnabled = Debug.isDebugBuild;
+        }
+#endif
+    }
+}
