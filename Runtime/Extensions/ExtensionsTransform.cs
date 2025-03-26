e00cfb541f3d131c67e0bcfd83e4bc7677340296
Cameron <cameo221@gmail.com>
2025-03-25 22:57:34 -0700

    add set x, y, z

--- Runtime/Extensions/ExtensionsTransform.cs
+++ Runtime/Extensions/ExtensionsTransform.cs
@@ -13,5 +13,35 @@ namespace CamLib
         {
             transform.position = new Vector3(position.x, position.y, transform.position.z);
         }
+        
+        public static void SetX(this Transform transform, float x)
+        {
+            transform.position = new Vector3(x, transform.position.y, transform.position.z);
+        }
+        
+        public static void SetY(this Transform transform, float y)
+        {
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        }
+        
+        public static void SetZ(this Transform transform, float z)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, z);
+        }
+
+        public static void SetLocalX(this Transform transform, float x)
+        {
+	        transform.localPosition = new Vector3(x, transform.localPosition.y, transform.localPosition.z);
+        }
+        
+        public static void SetLocalY(this Transform transform, float y)
+        {
+	        transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+        }
+        
+        public static void SetLocalZ(this Transform transform, float z)
+        {
+	        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, z);
+        }
     }
 }
