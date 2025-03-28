e1c2cabc405b32ae6a266db9c42f6eb061d2558e
Cameron <cameo221@gmail.com>
2025-03-28 00:20:28 -0700

    random anim offset

---
+++ Runtime/Systems/AnimationLoopRandomOffset.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Game
+{
+    /// <summary>
+    /// Simple component that Makes an animator play with a random offset. For example, if multiple flags placed next to each other should have variety 
+    /// </summary>
+    public class AnimationLoopRandomOffset : MonoBehaviour
+    {
+        [SerializeField] private Animator _anim = null;
+        [SerializeField] private string _animName = "";
+
+        private void Start()
+        {
+            if (_anim != null)
+            {
+                _anim.Play(_animName, 0, Random.value);
+            }
+        }
+    }
+}
