c8676337037be02616726c2f3bfcf2dcd70a55a8
Cameron <cameo221@gmail.com>
2025-06-24 13:40:00 -0700

    canvas group stuff

--- Runtime/Extensions/ExtensionsMonoBehaviour.cs
+++ Runtime/Extensions/ExtensionsMonoBehaviour.cs
@@ -16,9 +16,16 @@ namespace CamLib
             return behaviour.StartCoroutine(routine);
         }
         
-        public static void SetVisible(this CanvasGroup group, bool active)
+        public static void SetVisible(this CanvasGroup group, bool visible)
+        {
+            group.alpha = visible ? 1 : 0;
+        }
+        
+        public static void SetActive(this CanvasGroup group, bool active)
         {
             group.alpha = active ? 1 : 0;
+            group.interactable = active;
+            group.blocksRaycasts = active;
         }
     }
 }
